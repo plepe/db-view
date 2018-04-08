6ebda4f3b3c8829adcc39ae5e8ff4501d7369258
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-04-08 11:25:06 +0200

    Mocha tests

---
+++ test/test.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const conf = JSON.parse(fs.readFileSync('test-conf.json', 'utf8'));
+
+const assert = require('assert')
+
+const DBApi = require('db-api')
+const DBView = require('../src/DBView')
+
+const dbApi = new DBApi(conf.url, conf.options)
+
+describe('DBView', () => {
+  it('init', () => {
+    new DBView(dbApi)
+  })
+
+  it('show', (done) => {
+    let view = new DBView(dbApi)
+    view.set_query({ table: 'test1' })
+    view.show((err, result) => {
+      assert.equal(err, null, 'Error should be null')
+      assert.equal(result, '[[{"a":2,"b":"bar","d":"b"},{"a":3,"b":"bla","d":"b"}]]')
+      done()
+    })
+  })
+})
