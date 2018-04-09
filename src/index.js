392477be247759cdcd352077a7fdb525bf6c483e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-04-09 06:24:34 +0200

    test.html / src/index.js: rewrite as twig view

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const DBApi = require('db-api')
-const DBView = require('./DBView.js')
+const DBViewTwig = require('./DBViewTwig')
+const twig = require('twig')
 
 window.onload = () => {
   let api = new DBApi('api.php')
-  let view = new DBView(api)
-  view.set_query({ table: 'test1' })
+  let view = new DBViewTwig(api, 'Entry {{ entry.id }}: {{ entry.commentsCount }} Comments\n<ul>{% for c in entry.comments %}<li>{{ c.text }}</li>{% endfor %}</ul>', { twig })
+  view.set_query({ table: 'test2' })
   view.show((err, result) => {
     document.body.innerHTML = result
   })
